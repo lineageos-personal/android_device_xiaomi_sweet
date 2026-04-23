09252a2f2f4a5301a40a54ad45024d92ea9f2304
blazerpaul15 <blazerpaul15@gmail.com>
2026-04-23 18:06:25 +0900

    sweet: Include gapps

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Gapps
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
